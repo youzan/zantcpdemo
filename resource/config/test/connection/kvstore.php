9c15719c7de75983583d879c27520e289e63dc75
demon <demon@php.net>
2016-05-10 15:16:38 +0800

    pool

--- resource/config/test/connection/kvstore.php
+++ resource/config/test/connection/kvstore.php
@@ -5,19 +5,19 @@
  * Date: 16/4/5
  * Time: 下午4:53
  */
+
 return [
     'test' => [
         'engine'=> 'kVStore',
+        'ip_list' => '192.168.66.202:3000',
+        'user' => '',
+        'password' => '',
+        'policy' => [
+            'timeout' => 1000,
+        ],
         'pool'  => [
-            'pool_name' => 'test',
             'keeping-sleep-time' => 10000,
             'init-connection'=> 1,
-            'ip_list' => '192.168.66.202:3000',
-            'user' => '',
-            'password' => '',
-            'policy' => [
-                'timeout' => 1000,
-            ],
         ],
     ],
 ];
