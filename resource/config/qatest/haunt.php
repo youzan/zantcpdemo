bf4d285ab4112ac8d69574c48e2ada4259e66e5c
小牛 <xiaoniu@qima-inc.com>
2016-06-21 17:15:18 +0800

    server discovery

---
+++ resource/config/qatest/haunt.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoniu
+ * Date: 16/6/16
+ * Time: 下午4:03
+ */
+return [
+    'app_names' => [
+        'TcpDemo'
+    ],
+    'discovery' => [
+        'host' => 'http://etcd0-qa.s.qima-inc.com',
+        'port' => 2379,
+        'timeout' => 35000,
+        'uri' => '/v2/keys',
+        'protocol' => 'nova',
+        'namespace' => 'com.youzan.service',
+        'loop_time' => 1000,
+    ],
+    'watch' => [
+        'host' => 'http://etcd0-qa.s.qima-inc.com',
+        'port' => 2379,
+        'timeout' => 30000,
+        'uri' => '/v2/keys',
+        'protocol' => 'nova',
+        'namespace' => 'com.youzan.service',
+        'loop_time' => 5000,
+    ],
+    'watch_store' => [
+        'loop_time' => 1000,
+    ],
+    'register' => [
+        'host' => '127.0.0.1',
+        'port' => 8687,
+        'uri' => '/register',
+        'timeout' => 10000,
+        'protocol' => 'nova',
+        'namespace' => 'com.youzan.service',
+    ],
+];
---
+++ resource/config/qatest/nova.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'server' => [
+        'daemonize' => 0,
+        'worker_num' => 2,
+        'max_request' => 100000,
+        'reactor_num' => 2,
+        'open_length_check' => 1,
+        'package_length_type' => 'N',
+        'package_length_offset' => 0,
+        'package_body_offset' => 0,
+        'open_nova_protocol' => 1,
+        'package_max_length' => 2000000
+    ],
+    'novaApi' => [
+        'path'  => 'vendor/zanphp/novatcpdemo/gen-php',
+        'namespace' => 'Com\\Youzan\\NovaTcpDemo\\',
+    ],
+
+    'platform' => [
+        'enable_report' => 0,
+        'hawk_url' => 'http://192.168.66.240:8188',
+        'report_interval' => 300
+    ],
+];
